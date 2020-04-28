3355f46dcd603fce0055a92d24812d294d34af8d
Craig Hays <craig@craighays.com>
2020-04-28 15:16:08 +0100

    first working publicly viewable version, probably full of bugs but it works for me so far

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 here=`pwd`
-elasticServer=http://192.168.1.200:9200/mitmproxy/_doc
+elasticServer=http://your.elastic.server.local:9200/mitmproxy/_doc
 storeBinaryContent=false
 
 docker run --rm -it -p 8080:8080 -v $here/.mitmproxy:/home/mitmproxy/.mitmproxy -v $here/scripts:/scripts/ craighays/mitmproxy-pythonrequests mitmdump -s /scripts/elasticArchive.py --set elasticsearch_URL=$elasticServer --set storeBinaryContent=$storeBinaryContent
